8de57ef6b5dc9e1fc6c20bfaacf9e2c0c32c5583
NeoByte <thatneobyte@gmail.com>
2026-02-27 00:29:54 +0100

    updated version to match change

--- TheLounge/TheLounge-ShoutboxBeautifier.Android.user.js
+++ TheLounge/TheLounge-ShoutboxBeautifier.Android.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            The Lounge – Shoutbox Beautifier (Android) (ThatNeoByte Edition)
 // @namespace       https://github.com/ThatNeoByte/UserScripts
-// @version         2.7-tnb.9
+// @version         2.7-tnb.10
 // @description     Advanced rework of the original Shoutbox Beautifier for The Lounge. Reformats bridged chatbot messages to appear as native user messages, with extensible handler architecture, decorators, metadata-driven styling, regex matching, preview-safe DOM updates, and expanded network support.
 //
 // @author          spindrift
